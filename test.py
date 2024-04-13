7e8f517d46db5a16f5e153e9bc0a10f84cc07cf1
Caner Şermet <csermet@ankasoft.co>
2024-04-14 02:04:23 +0300

    bluetooth

--- test.py
+++ test.py
@@ -1,47 +1,32 @@
-import gpiod
-import time     
-import spidev
-from nrf24 import NRF24
-from machine import Pin, SPI
-from digitalio import DigitalInOut, Direction, Pull
+import bluetooth
 
-
-pipes = (b'\xe1\xf0\xf0\xf0\xf0', b'\xd2\xf0\xf0\xf0\xf0')
-
-spi = SPI(0, sck=Pin(6), mosi=Pin(7), miso=Pin(4))
-csn = Pin(14, mode=Pin.OUT, value=1)
-ce = Pin(17, mode=Pin.OUT, value=0)
-radio = NRF24(spi, csn, ce, channel=100, payload_size=32)
-
-radio.begin(0, 17)
-
-radio.setPayloadSize(32)
-radio.setChannel(0x76)
-radio.setDataRate(NRF24.BR_2MBPS)
-radio.setPALevel(NRF24.PA_MIN)
-
-radio.setAutoAck(True)
-radio.enableDynamicPayloads()
-radio.enableAckPayload()
-
-radio.openWritingPipe(pipes[0])
-radio.printDetails()
-
-sendMessage = list("Hi..Arduino UNO")
-while len(sendMessage) < 32:
-    sendMessage.append(0)
-
-while True:
-    start = time.time()
-    radio.write(sendMessage)
-    print("Sent the message: {}".format(sendMessage))
-    radio.startListening()
-
-    while not radio.available(0):
-        time.sleep(1/100)
-        if time.time() - start > 2:
-            print("Timed out.")  # print error message if the radio disconnected or not functioning anymore
-            break
-
-    radio.stopListening()     # close radio
-    time.sleep(3)  # give delay of 3 seconds
+def receiveMessages():
+  server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+  
+  port = 1
+  server_sock.bind(("",port))
+  server_sock.listen(1)
+  
+  client_sock,address = server_sock.accept()
+  print "Accepted connection from " + str(address)
+  
+  data = client_sock.recv(1024)
+  print "received [%s]" % data
+  
+  client_sock.close()
+  server_sock.close()
+  
+def sendMessageTo(targetBluetoothMacAddress):
+  port = 1
+  sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+  sock.connect((targetBluetoothMacAddress, port))
+  sock.send("hello!!")
+  sock.close()
+  
+def lookUpNearbyBluetoothDevices():
+  nearby_devices = bluetooth.discover_devices()
+  for bdaddr in nearby_devices:
+    print str(bluetooth.lookup_name( bdaddr )) + " [" + str(bdaddr) + "]"
+    
+    
+lookUpNearbyBluetoothDevices()
