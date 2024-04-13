697925fbb084eef40f460435f66eb18c735e67c1
Caner Şermet <csermet@ankasoft.co>
2024-04-14 02:20:03 +0300

    bluetooth

--- test.py
+++ test.py
@@ -1,36 +1,10 @@
-import bluetooth, subprocess
-
-def receiveMessages():
-  server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+import bluetooth
   
-  port = 1
-  server_sock.bind(("",port))
-  server_sock.listen(1)
-  
-  client_sock,address = server_sock.accept()
-  print("Accepted connection from " + str(address))
-  
-  data = client_sock.recv(1024)
-  print("received [%s]" % data)
-  
-  client_sock.close()
-  server_sock.close()
-  
-def sendMessageTo(targetBluetoothMacAddress):
-    # kill any "bluetooth-agent" process that is already running
-    subprocess.call("kill -9 `pidof bluetooth-agent`",shell=True)
-    # Start a new "bluetooth-agent" process where XXXX is the passkey
-    subprocess.call("bluetooth-agent " + "1863" + " &",shell=True)
-    port = 1
-    sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-    sock.connect((targetBluetoothMacAddress, port))
-    sock.send("hello!!")
-    sock.close()
-  
-def lookUpNearbyBluetoothDevices():
-  nearby_devices = bluetooth.discover_devices()
-  for bdaddr in nearby_devices:
+nearby_devices = bluetooth.discover_devices()
+for bdaddr in nearby_devices:
     print(str(bluetooth.lookup_name( bdaddr )) + " [" + str(bdaddr) + "]")
-    
-    
-sendMessageTo("00:21:09:00:21:87")
+
+sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+sock.connect(("00:21:09:00:21:87", 1))
+sock.send("hello!!")
+sock.close()
