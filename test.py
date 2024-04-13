5ef4f8d5c81f7da0e534ff0184a80d637937a7b2
Caner Şermet <csermet@ankasoft.co>
2024-04-14 02:59:22 +0300

    bluetooth

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-import bluetooth
-  
-nearby_devices = bluetooth.discover_devices()
-for bdaddr in nearby_devices:
-    print(str(bluetooth.lookup_name( bdaddr )) + " [" + str(bdaddr) + "]")
+import bluetoot
 
 sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 sock.connect(("00:21:09:00:21:87", 1))
