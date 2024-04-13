84b4f67ab97d5d87dbf1a0d1b22289da77eef3d9
Caner Şermet <csermet@ankasoft.co>
2024-04-14 02:06:02 +0300

    bluetooth

--- test.py
+++ test.py
@@ -26,7 +26,7 @@ def sendMessageTo(targetBluetoothMacAddress):
 def lookUpNearbyBluetoothDevices():
   nearby_devices = bluetooth.discover_devices()
   for bdaddr in nearby_devices:
-    print str(bluetooth.lookup_name( bdaddr )) + " [" + str(bdaddr) + "]"
+    print(str(bluetooth.lookup_name( bdaddr )) + " [" + str(bdaddr) + "]")
     
     
 lookUpNearbyBluetoothDevices()
