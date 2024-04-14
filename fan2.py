b751fa4675a9b690e1248da17e0678fdb1901140
Caner Şermet <csermet@ankasoft.co>
2024-04-15 01:27:28 +0300

    fan

--- fan2.py
+++ fan2.py
@@ -1,8 +1,22 @@
-import socket
+import RPi.GPIO as GPIO
 import time
-from time import sleep
-import machine
 
-GPIO_PIN_9 = machine.Pin(9)
-pwm9 = machine.PWM(GPIO_PIN_9)
-pwm9.freq(25000)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+IN1=20
+IN2=21
+ENA=16
+
+GPIO.setup(IN1,GPIO.OUT)
+GPIO.setup(IN2,GPIO.OUT)
+GPIO.setup(ENA,GPIO.OUT)
+GPIO.output(IN1,GPIO.LOW)
+GPIO.output(IN2,GPIO.LOW)
+
+p=GPIO.PWM(ENA,1000)
+p.start(0)
+
+GPIO.output(IN1,GPIO.HIGH)
+GPIO.output(IN2,GPIO.LOW)
+p.ChangeDutyCycle(100)
