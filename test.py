f1611c80638ed65c3b0414c5de1d20963d0601d4
Caner Şermet <csermet@ankasoft.co>
2024-04-14 03:17:58 +0300

    bluetooth

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 sock.connect(("00:21:09:00:21:87", 1))
 sock.send("send")
 
-sock.bind(("00:21:09:00:21:87",1))
-sock.listen(1)
+#sock.bind(("00:21:09:00:21:87",1))
+#sock.listen(1)
 
 client_sock,address = sock.accept()
 print("Accepted connection from " + str(address))
