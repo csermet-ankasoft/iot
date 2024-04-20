edb2919f7467f1aacce23931afb1e63a848fb51e
Caner Şermet <csermet@ankasoft.co>
2024-04-20 20:37:55 +0300

    bluetootTest

--- raspberry/bluetoothTest.py
+++ raspberry/bluetoothTest.py
@@ -1,12 +1,12 @@
 import serial
 import time
 
-port = serial.Serial("/dev/rfcomm0", baudrate=9600) # reading and writing data from and to arduino serially.
+port = serial.Serial("/dev/rfcomm1", baudrate=9600) # reading and writing data from and to arduino serially.
                                                     # rfcomm0 -> this could be different
 
 while True:
     print("DIGITAL LOGIC -- > SENDING...")
-    port.write("3")
+    port.write(3)
     rcv = port.readline()
     if rcv:
         print(rcv)
