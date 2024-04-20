ebb5784e90c2ea661467088042b7ce159d953713
Caner Şermet <csermet@ankasoft.co>
2024-04-21 00:03:49 +0300

    dhtTest

--- raspberry/DHT22Test.py
+++ raspberry/DHT22Test.py
@@ -1,19 +1,29 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
 import time
 import board
 import adafruit_dht
 
-# Sensor data pin is connected to GPIO 4
-sensor = adafruit_dht.DHT22(board.D4)
-# Uncomment for DHT11
-#sensor = adafruit_dht.DHT11(board.D4)
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT22(board.D18)
+
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
 while True:
     try:
         # Print the values to the serial port
-        temperature_c = sensor.temperature
+        temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
-        humidity = sensor.humidity
-        print("Temp={0:0.1f}ºC, Temp={1:0.1f}ºF, Humidity={2:0.1f}%".format(temperature_c, temperature_f, humidity))
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
 
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
@@ -21,7 +31,7 @@ while True:
         time.sleep(2.0)
         continue
     except Exception as error:
-        sensor.exit()
+        dhtDevice.exit()
         raise error
 
-    time.sleep(3.0)
+    time.sleep(2.0)
