79537bf2813518e4d5173f5faf8c933254329f7e
Caner Şermet <csermet@ankasoft.co>
2024-04-28 00:10:03 +0300

    main error fix

--- raspberry/function/mq135.py
+++ raspberry/function/mq135.py
@@ -4,7 +4,7 @@ import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
 i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS.ADS1115(i2c)
+ads = ADS.ADS1115(i2c, address=0x48)
 
 def getAirQuality():
     try:
